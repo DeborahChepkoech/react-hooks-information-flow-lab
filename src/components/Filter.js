a4756b6977a2f97ad539912968f973d5d15bdbed
deborahchepkoech <deborah.chepkoech@student.moringaschool.com>
2025-04-14 12:43:50 +0300

    Information flow

---
+++ src/components/Filter.js
@@ -0,0 +1,12 @@
+import React from "react";
+function Filter({ selectedCategory, onCategoryChange }){
+    return (
+        <select name ="filter" value={selectedCategory} onChange={onCategoryChange}>
+        <option value="All">Filter by category</option>
+          <option value="Produce">Produce</option>
+          <option value="Dairy">Dairy</option>
+          <option value="Dessert">Dessert</option>
+        </select>
+    )
+}
+export default Filter;
